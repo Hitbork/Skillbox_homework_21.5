5dafd1dac50ce32d4cdc6aa27e1d37295f950215
Hitbork <hitbork@yandex.ru>
2024-03-01 07:51:22 +0300

    Exercise 1.1.0

---
+++ Exercise_1/cmake-build-debug/CMakeFiles/Exercise_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise_1.dir/main.cpp.obj"
+  "CMakeFiles/Exercise_1.dir/main.cpp.obj.d"
+  "Exercise_1.exe"
+  "Exercise_1.exe.manifest"
+  "Exercise_1.pdb"
+  "libExercise_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exercise_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
