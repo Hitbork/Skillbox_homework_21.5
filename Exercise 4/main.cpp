717f1c2f33ae336f5f09295335ba9e0d9e560a89
hitbork <hitbork@yandex.ru>
2024-05-07 18:06:26 +0300

    snutvh

--- Exercise 4/main.cpp
+++ Exercise 4/main.cpp
@@ -3,8 +3,7 @@
 #include <ctime>
 #include <vector>
 
-bool map[20][20],
-    isGameEnded = false;
+bool isGameEnded = false;
 
 
 struct Character {
@@ -14,6 +13,8 @@ struct Character {
         damage = 0,
         x = 0,
         y = 0;
+
+    Character() {}
 
     Character(int number) {
         if (number == 0) {
@@ -27,29 +28,35 @@ struct Character {
         this->health = 50 + rand() % 101;
         this->armor = rand() % 51;
         this->damage = 15 + rand() % 16;
-        //Здесь должна быть проверка на то, что занято ли поле
-        asdasd
-        this->x = rand() % 20;
-        this->y = rand() % 20;
+        Character isFieldBusy = NULL;
+        do {
+            this->x = rand() % 20;
+            this->y = rand() % 20;
+            isFieldBusy = map[this->x][this->y];
+            if (isFieldBusy == NULL)
+                map[this->x][this->y] = this;
+        } while (isFieldBusy != NULL);
+            
     }
 };
 
-Character player(0);
-std::vector<Character> enemies;
 
+
+
+Character player(0);
+Character map[20][20];
 
 
 void create_enemies() {
     for (int i = 1; i < 6; ++i) {
         Character enemy(i);
-        enemies.push_back(enemy);
     }
 }
 
 void filling_map() {
     for (int i = 0; i < 20; ++i) {
         for (int j = 0; j < 20; ++j) {
-            map[i][j] = false;
+            map[i][j] = NULL;
         }
     }
 }
@@ -57,7 +64,7 @@ void filling_map() {
 void map_output() {
     for (int i = 0; i < 20; ++i) {
         for (int j = 0; j < 20; ++j) {
-            if (!map[i][j])
+            if (map[i][j] == player)
                 std::cout << ".";
             else {
                 if (player.x == i && player.y == j)
@@ -74,7 +81,6 @@ void map_output() {
 
 void create_new_game() {
     filling_map();
-    map[player.x][player.y] = true;
     create_enemies();
 }
 
